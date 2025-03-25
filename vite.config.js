35469bafa3ca7e872917cd376d9e977a18d4781c
Leogelv <gelvihleonid@gmail.com>
2025-03-25 16:41:00 +0700

    Исправлены конфигурации для деплоя на Vercel

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,13 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+  },
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
